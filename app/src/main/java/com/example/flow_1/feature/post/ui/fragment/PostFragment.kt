9fa8f1a2e40545ea4439edb26e0b64c301447d33
mahdi_homePC <mahditavakoli7878@gmail.com>
2022-11-18 18:45:14 +0330

    init project

--- app/src/main/java/com/example/flow_1/feature/post/ui/fragment/PostFragment.kt
+++ app/src/main/java/com/example/flow_1/feature/post/ui/fragment/PostFragment.kt
@@ -4,15 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.example.flow_1.R
-import com.example.flow_1.databinding.FragmentPostBinding
 import com.example.flow_1.feature.post.ui.adapter.PostAdapter
 
 class PostFragment : Fragment() {
 
-    private lateinit var binding: FragmentPostBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -20,21 +18,17 @@ class PostFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        binding = DataBindingUtil.inflate(
-            inflater,
-            R.layout.fragment_post,
-            container, false
-        )
-        return binding.root
+    ): View? {
+        return inflater.inflate(R.layout.fragment_post, container, false)
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val postAdapter = PostAdapter()
-        binding.apply {
-            rvPostsPostFragment.adapter = postAdapter
-        }
+        val rvPostsPostFragment = view.findViewById<RecyclerView>(R.id.rv_posts_postFragment)
+        rvPostsPostFragment.adapter = postAdapter
+
         //TODO : submit list
     }
 
